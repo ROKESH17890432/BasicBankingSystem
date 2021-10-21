1e44a2684a6b0f1b2830fd38c7241261e1de0400
ROKESH17890432 <90196970+ROKESH17890432@users.noreply.github.com>
2021-10-21 15:59:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/navbar.css">
+    
+    <title>Basic Banking System</title>  
+</head>
+<style>
+    body{
+        background-color:lightblue;
+        background-repeat:repeat;
+        background-size:100%;
+    }
+    </style>
+<body>
+    <?php
+    include 'navbar.php';
+    ?>
+
+    <!-- Start Main area -->
+    <!-- Start About area -->
+    <section id="about" class="site-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12  about-image">
+                  <img src="img/bank1.png" alt="banner-img" class="img-fluid">
+                    
+                </div>
+                <div class="col-lg-6 col-md-12 about">
+                  <h2><br><center><b>BANK OF SPARKS</b></center></h2>
+                  <h4 class="title-text">Our Bank consists of many activities that can be done through a number of financial institutions that accept deposits from individuals and other entities, and then use this money to offer loans, and to invest and earn profit.
+                    <br><br>It is an project under the The Sparks foundation as a part of intersnship.In which a dynamic website has to be created with dummy 10 users and transaction to be made between them.<br>
+                    <br>Here I have used HTML,CSS as a frontend and PHP as backend and MYSQL as a database. 
+                  <br>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About area -->
+
+    <footer class="text-center mt-5 py-2">
+        <p>&copy 2021. Made by <b>ROKESH A</b> <br> For the Project of  <b>THE SPARKS FOUNDATION</b></p>
+    </footer>
+</body>
+</html>
+ 
+    
