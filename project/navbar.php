60c796d2a764180047bf568759ee52c34669e4b1
ROKESH17890432 <90196970+ROKESH17890432@users.noreply.github.com>
2021-10-21 16:21:04 +0530

    Add files via upload

---
+++ project/navbar.php
@@ -0,0 +1,62 @@
+<!-- navbar --> 
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+
+        body{margin-top: 15px;
+		background-color: lightblue;
+        }
+		.collapse navbar-collapse
+		{
+			background-color:#333;
+			overflow:hidden;
+		}
+		.collapse navbar-collapse a
+		{
+			float :left;
+			color : #f2f2f2;
+			text-align : center;
+			padding : 14px 16px;
+			text-decoration:none;
+			font-size:17px;
+		}
+          .btn {
+                  border: 2px white;
+                  background-color: #333;
+                  color: #5bc0de;
+                  padding: 14px 25px;
+                  font-size: 16px;
+                  cursor: pointer;
+                  margin: 6px;
+                }
+            
+            .df:hover {
+        color: white;
+        font-size: 16px;
+    background-color: #ddd;
+background-image: linear-gradient(315deg,  #5bc0de 0%,  #000000 74%);
+
+}
+
+        </style>
+      </head>
+      <body>
+      
+      <nav  class="navbar navbar-expand-md navbar-light bg-info">
+      <a class="navbar-brand" href="index.php"><h3><b>BASIC BANKING SYSTEM</b></h3></a>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">HOME</a>
+              </li>
+              
+              
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="about.php">ABOUT</a>
+              </li>
+          </div>
+       </nav>
+     </body>
+     </html>
